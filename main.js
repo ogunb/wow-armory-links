ac26ca65acb78390291dcca24ade05138e646bc8
Ogun Babacan <ogun.babacan@trendyol.com>
2022-03-26 22:51:03 +0300

    feat(raider.io): add shortcut

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
+const EXTENSION_URL = 'chrome-extension://mldagnjieibhfkdhfncjflkennfcheag';
+
 let retryCount = 0;
 let interval = setInterval(() => {
-  const nameWrapper = document.querySelector('.CharacterHeader-nameTitle');
+  const header = document.querySelector('.CharacterHeader-character');
 
-  if (retryCount > 10 || nameWrapper !== null) {
+  if (retryCount > 10 || header !== null) {
     clearInterval(interval);
   }
 
-  if (nameWrapper !== null) {
-    requestIdleCallback(() => addRaiderIOLink(nameWrapper));
+  if (header !== null) {
+    requestIdleCallback(() => addRaiderIOLink(header));
   }
 
-  retryCount++
+  retryCount++;
 }, 500);
 
-async function addRaiderIOLink(wrapper) {
-  console.log(wrapper);
+async function addRaiderIOLink(header) {
+  header.insertAdjacentHTML(
+    'beforeend',
+    createNameArea(
+      createLogoLink({
+        href: getRaiderIOHref(),
+        image: { url: `${EXTENSION_URL}/images/raider-io.svg`, alt: 'raider-io-logo' },
+        isLast: true,
+      }),
+    ),
+  );
+}
+
+function getRaiderIOHref() {
+  const [, respectivePath] = window.location.pathname.split('character/');
+  return `https://raider.io/characters/${respectivePath}`;
+}
+
+function createNameArea(...links) {
+  return `
+    <div class="CharacterHeader-nameArea">
+      <div class="CharacterHeader-nameTitle flex flex-items-center" style="min-height: 53px;">
+        ${links.join(' ')}
+      </div>
+    </div>
+  `;
+}
+
+function createLogoLink({ image, href, isLast }) {
+  const imageClasses = isLast ? '' : 'class="margin-right-double-tiny"';
+
+  return `
+    <a href="${href}" target="_blank" rel="noreferrer">
+      <img src="${image.url}" alt="${image.alt}" width="24" height="24" ${imageClasses} />
+    </a>
+  `;
 }
